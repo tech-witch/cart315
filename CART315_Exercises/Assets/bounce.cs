9adcedb47b8049ca3032de02d21d8e773d5ed660
Liane <lianedecarychen@gmail.com>
2020-01-22 12:16:24 -0500

    Added Bounce Script

---
+++ CART315_Exercises/Assets/bounce.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bounce : MonoBehaviour
+{
+	public GameObject myObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnTriggerStay(Collider other)
+	{
+		other.GetComponent<Rigidbody>().AddForce(0,700.0f*Time.deltaTime,0);
+	}
+    private void OnTriggerEnter(Collider other)
+	{
+        if (other.gameObject == myObject)
+		{
+			other.GetComponent<AudioSource>().Play();
+		}
+		
+	}
+}
--- CART315_Exercises/Assets/rotate.cs
+++ CART315_Exercises/Assets/rotate.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class rotate : MonoBehaviour
 {
+	public float rotate_speed = 1.0f;
+
 	// Start is called before the first frame update
 	void Start()
 	{
@@ -13,6 +15,10 @@ public class rotate : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-		this.transform.Rotate(0,1.0f,0);
+        if (Input.GetButton("Fire1"))
+		{
+			this.transform.Rotate(0.0f, rotate_speed, 0.0f);
+		}
+		
     }
 }
